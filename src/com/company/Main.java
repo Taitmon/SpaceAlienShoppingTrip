632addaafec51b58e391307bea9556f7c58b9daa
taitmon <taitmonlynch@gmail.com>
2019-01-23 10:37:59 -0600

    check in

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        printHello();
+        goShopping();
+        printGoodbye();
+    }
+
+    private static void printHello()
+    {
+        System.out.println("Hello, Earthlings!");
+    }
+
+    private static void printGoodbye()
+    {
+        System.out.println("Goodbye, Earthlings!");
+    }
+
+    private static void goShopping()
+    {
+        System.out.println("Shopping like it's 1999!");
+    }
+}
